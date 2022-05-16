8f2458e13e9ac6034f3e97bbf66759178a1948a2
half-metal-pc <halfmetal@gmail.com>
2022-05-15 23:58:32 -0700

    update readme

--- src/lib/agentConnections.js
+++ src/lib/agentConnections.js
@@ -1,3 +1,4 @@
+//& add agents here
 export const agentConnections = [
     {env:"node non-prod",location:"http://10.5.28.21:8082",user:"user",pw:"pw"},
     {env:"sveltekit non-prod",location:"http://10.5.28.21:3000",user:"user",pw:"pw"}
